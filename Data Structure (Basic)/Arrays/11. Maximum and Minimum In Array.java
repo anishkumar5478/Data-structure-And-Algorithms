8ade5821cfcd6529db1f3aca4ce2d3e926c1def2
Anish Kumar <57875082+anishkumar5478@users.noreply.github.com>
2022-01-03 21:20:05 +0530

    Create 11. Maximum and Minimum In Array.java

---
+++ Data Structure (Basic)/Arrays/11. Maximum and Minimum In Array.java
@@ -0,0 +1,78 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driver
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine());
+            String str[] = read.readLine().trim().split(" ");
+            
+            int input[] = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                input[i] = Integer.parseInt(str[i]);
+            }
+            
+            //int x = Integer.parseInt(read.readLine());
+            Get obj = new Get();
+            System.out.println(obj.maximumElement(input, n) + " " + obj.minimumElement(input, n));
+            
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Get
+{
+    public static int maximumElement(int arr[],int n)
+    {
+        // Your code here
+        
+        int max = arr[0];
+        int i ;
+        for( i=0;i<n;i++)
+        {
+            if(arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+        
+       return max;
+    }
+    
+    public static int minimumElement(int arr[],int n)
+    {
+        // YOUR code here
+        
+                
+        int min = arr[0];
+        int j ;
+        for( j=0;j<n;j++)
+        {
+            if(arr[j] < min)
+            {
+                min = arr[j];
+            }
+        }
+        
+       return min;
+    }
+}
+
