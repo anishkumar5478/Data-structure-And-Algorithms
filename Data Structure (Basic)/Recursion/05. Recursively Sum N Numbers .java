69127c085fafbe6e808b4c82197f0c0ffa4d18a6
Anish Kumar <57875082+anishkumar5478@users.noreply.github.com>
2022-01-07 11:17:24 +0530

    Create 05. Recursively Sum N Numbers .java

---
+++ Data Structure (Basic)/Recursion/05. Recursively Sum N Numbers .java
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine());
+            
+            System.out.println(new Solution().recursiveSum(n));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    // Complete the function
+    // N: input element
+    public static int recursiveSum(int N)
+    {
+        // recursively sum from 1 to N
+        if(N==0 )
+            return 0;
+            
+        else
+        {
+            return (N + recursiveSum(N-1));
+        }
+    }
+}
