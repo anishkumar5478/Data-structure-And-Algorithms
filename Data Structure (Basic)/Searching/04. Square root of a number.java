d506149e1b1e30e393f30b552dc9f1e0eeb0c12a
Anish Kumar <57875082+anishkumar5478@users.noreply.github.com>
2022-01-12 20:10:32 +0530

    Create 04. Square root of a number.java

---
+++ Data Structure (Basic)/Searching/04. Square root of a number.java
@@ -0,0 +1,43 @@
+// { Driver Code Starts
+import java.util.Scanner;
+
+class SquartRoot
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			long a = sc.nextInt();
+			Solution obj = new Solution();
+			System.out.println(obj.floorSqrt(a));
+		t--;
+		}
+	}
+}// } Driver Code Ends
+
+
+/*You are required to complete
+this function*/
+
+// Function to find square root
+// x: element to find square root
+class Solution
+{
+     long floorSqrt(long x)
+	 {
+		// Your code here
+		int i=1;
+		while(true)
+		{
+		    
+		    if(i*i >x)
+		        return i-1;
+		    if(i*i == x)
+		        return i;
+		  
+		    i++;      
+		}
+	 }
+}
