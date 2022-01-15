4efb4df3dea39dcc4150668cb26e6ff6c2c677d0
Anish Kumar <57875082+anishkumar5478@users.noreply.github.com>
2022-01-15 15:28:16 +0530

    Create 02.Selection -sort.java

---
+++ Data Structure (Basic)/Sorting/02.Selection -sort.java
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+import java.util.*;
+
+class GFG
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n]; 
+			for(int i=0;i<n;i++)
+			{
+				arr[i] = sc.nextInt();
+			}
+		
+			Solution obj = new Solution();
+			obj.selectionSort(arr, n);
+			
+			for(int i=0;i<n;i++)
+		    	System.out.print(arr[i]+" ");
+		    System.out.println();
+			t--;
+		}
+		
+	}
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+	int  select(int arr[], int i)
+	{
+        // code here such that selectionSort() sorts arr[]
+          return arr[0];
+	}
+	
+	void selectionSort(int arr[], int n)
+	{
+	    for(int i = 0; i < n - 1; i++)
+	    {
+	        for(int j = i + 1; j < n; j++){
+	            if(arr[i] > arr[j]){
+	                int temp = arr[j];
+	                arr[j] = arr[i];
+	                arr[i] = temp;
+	            }
+	        }
+	         
+	    }
+	}
+}
